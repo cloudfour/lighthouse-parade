71c0f2ef6025cc230c6f237a55f599224912ff1a
Caleb Eby <caleb.eby01@gmail.com>
2020-10-21 12:14:33 -0700

    Add eslint disable comment and explanation

--- lighthouse.ts
+++ lighthouse.ts
@@ -2,6 +2,9 @@ import { spawnSync } from 'child_process';
 
 const lighthouseCli = require.resolve('lighthouse/lighthouse-cli');
 
+// This function is marked as async to wrap it with a promise to make error handling easier
+// Even though the underlying process is spawned synchronously
+// eslint-disable-next-line @cloudfour/typescript-eslint/require-await
 export const runLighthouseReport = async (url: string) => {
   const { status = -1, stdout } = spawnSync('node', [
     lighthouseCli,
