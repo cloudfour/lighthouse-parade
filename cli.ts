fc4f06011ddf016eceae9269a867e30a430f9d17
Caleb Eby <caleb.eby01@gmail.com>
2021-01-12 15:32:26 -0800

    Add examples to --max-crawl-depth help text

--- cli.ts
+++ cli.ts
@@ -61,7 +61,10 @@ sade('lighthouse-parade <url> [dataDirectory]', true)
     'Control the maximum number of ligthhouse reports to run concurrently',
     os.cpus().length - 1
   )
-  .option('--max-crawl-depth', 'Control the maximum depth of crawled links')
+  .option(
+    '--max-crawl-depth',
+    'Control the maximum depth of crawled links. 1 means only the entry page will be used. 2 means the entry page and any page linked directly from the entry page will be used.'
+  )
   .option(
     '--include-path-glob',
     'Specify a glob (in quotes) for paths to match. Links to non-matched paths will not be crawled. The entry page will be crawled regardless of this flag. This flag can be specified multiple times to allow multiple paths. `*` matches one url segment, `**` matches multiple segments. Trailing slashes are ignored.'
