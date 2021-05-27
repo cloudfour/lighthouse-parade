513cd1f8130128af5f156bd71e2ca0f676e6f523
Caleb Eby <caleb.eby01@gmail.com>
2021-05-27 09:35:14 -0700

    Prettier

--- emitter.ts
+++ emitter.ts
@@ -14,7 +14,7 @@ export const createEmitter = <Events extends EventMap, Resolve = never>() => {
     // the new event listeners are fired
     Promise.resolve().then(() => {
       const handlers: Events[keyof Events][] = eventHandlers[eventName] || [];
-      for (const handler of handlers)  handler(...args);
+      for (const handler of handlers) handler(...args);
     });
   };
 
