66b1b6664283e51d1638043fdbfacd26fb9255bb
Caleb Eby <caleb.eby01@gmail.com>
2022-06-09 14:21:28 -0700

    Move combineOutputWriters to output-writer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import type { OutputWriter } from './output-writer/index.js';
 import {
+  combineOutputWriters,
   createGoogleSheetsOutputWriter,
   createCSVOutputWriter,
 } from './output-writer/index.js';
@@ -26,27 +27,7 @@ const getOutputWriter = async (outputs: string[]): Promise<OutputWriter> => {
     })
   );
 
-  // Used to make sure that the output writers are always updated with the same entry at the same time.
-  // Otherwise, there might be issues with output writers taking different amounts of time,
-  // and entries would be written in different orders.
-  let mutexPromise: Promise<unknown> = Promise.resolve();
-
-  const combinedOutputWriter: OutputWriter = {
-    addEntry: async (...args) => {
-      mutexPromise = mutexPromise.then(() =>
-        Promise.all(outputWriters.map((writer) => writer.addEntry(...args)))
-      );
-      await mutexPromise;
-    },
-    complete: async () => {
-      mutexPromise = mutexPromise.then(() =>
-        Promise.all(outputWriters.map((writer) => writer.complete()))
-      );
-      await mutexPromise;
-    },
-  };
-
-  return combinedOutputWriter;
+  return combineOutputWriters(outputWriters);
 };
 
 export const enum State {
--- src/output-writer/index.ts
+++ src/output-writer/index.ts
@@ -7,3 +7,28 @@ export interface OutputWriter {
   addEntry(report: LHR): Promise<void>;
   complete(): Promise<void>;
 }
+
+/** Create a single top-level OutputWriter that outputs to multiple OutputWriters */
+export const combineOutputWriters = (
+  outputWriters: OutputWriter[]
+): OutputWriter => {
+  // Used to make sure that the output writers are always updated with the same entry at the same time.
+  // Otherwise, there might be issues with output writers taking different amounts of time,
+  // and entries would be written in different orders.
+  let mutexPromise: Promise<unknown> = Promise.resolve();
+
+  return {
+    addEntry: async (...args) => {
+      mutexPromise = mutexPromise.then(() =>
+        Promise.all(outputWriters.map((writer) => writer.addEntry(...args)))
+      );
+      await mutexPromise;
+    },
+    complete: async () => {
+      mutexPromise = mutexPromise.then(() =>
+        Promise.all(outputWriters.map((writer) => writer.complete()))
+      );
+      await mutexPromise;
+    },
+  };
+};
